d377084891b080a9e8291e65d0e0e8c4d1899aa8
flecomte <flecomte@jeanlain.com>
2016-07-13 10:07:31 +0200

    add search.js

---
+++ Resources/public/js/search.js
@@ -0,0 +1,28 @@
+var search = function ($form)
+{
+    if (xhr_search != null) {
+        xhr_search.abort();
+    }
+    xhr_search = $.ajax({
+        url: window.location.pathname +'?'+ $form.serialize(),
+        type: 'GET',
+        dataType: 'html'
+    }).success(function (data, textStatus, jqXHR) {
+        $($form.data('destination')).html(data);
+        $form.trigger('searchFinish');
+    });
+};
+
+var xhr_search = null;
+$(document).on('keyup', '.search input', function (e) {
+    search($(this).parents('form'));
+});
+$(document).on('dp.change', '.search input', function (e) {
+    search($(e.target).parents('form'));
+});
+$(document).on('change', '.search select', function (e) {
+    search($(this).parents('form'));
+});
+$(function () {
+    $('.search button[type=submit]').hide();
+});
